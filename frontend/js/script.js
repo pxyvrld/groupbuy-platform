9a2463db4b96f06dcc747984602b7b594a943d72
pxyvlrd <priv.hubertjucha@gmail.com>
2025-11-16 23:15:28 +0100

    feat: add modal with campaign details and animated dynamic progress bars

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -40,10 +40,59 @@ timeElement.forEach((e) => {
 
 })
 
+//feat: Smooth scrolling on buttons
 
 let browseBtn = document.querySelector(".browseBtn");
+let browseNavBtn = document.querySelector("#browseNavBtn");
 let campaignsSection = document.querySelector("#campaigns");
 
 browseBtn.addEventListener("click", () => {
     campaignsSection.scrollIntoView({ behavior: 'smooth' });
 });
+
+browseNavBtn.addEventListener("click", () => {
+    campaignsSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+//feat: Modal
+
+const modalButtons = document.querySelectorAll(".detailsBtn")
+const modal = document.querySelector(".modal")
+const modalCloseBtn = document.querySelector(".modal-closeBtn")
+const modalOverlay = document.querySelector(".modal-overlay")
+const modalBody = document.querySelector(".modal-body")
+
+modalButtons.forEach((e) => {
+    e.addEventListener("click", () => {
+
+        const actualCard = e.closest(".card")
+        const actualBody = actualCard.outerHTML
+
+        modalBody.innerHTML = actualBody;
+        const metaButtonContainer = modalBody.querySelector(".card_detailsBtn")
+        metaButtonContainer.remove()
+        modal.classList.remove("hidden")
+    })
+})
+
+modalCloseBtn.addEventListener("click", () => {
+    modal.classList.add("hidden")
+})
+
+modalOverlay.addEventListener("click", (e) => {
+    if(e.target === modalOverlay) {
+        modal.classList.add("hidden")
+    }
+})
+
+
+//feat: Dynamic progress bars
+
+const progressBars = document.querySelectorAll(".progress");
+
+progressBars.forEach((bar) => {
+    const peopleText = bar.closest(".card").querySelector(".card_people_meter span").innerText;
+    const number = parseInt(peopleText.split("/")[0])
+    const capacity = parseInt(peopleText.split(" ")[0].split("/")[1])
+    bar.style.width = (number/capacity) * 100 + "%"
+})
